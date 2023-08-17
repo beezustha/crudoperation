7ef673904592042ef672db59c97f3a1035682695
Beezu Shrestha <beezustha@gmail.com>
2023-08-17 22:12:16 +0545

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -10,28 +10,21 @@ import User from "./Components/users/User";
 import { useState } from "react";
 
 function App() {
-    const [baseFilter, setFilter] = useState("");
+  const [baseFilter, setFilter] = useState("");
 
-    return (
-        <Router>
-            <div className="App">
-                <NavBar setfilter={setFilter} />
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Home searchThis={baseFilter} />}
-                    />
-                    <Route path="/users/add" element={<AddUser />} />
-                    <Route
-                        exact
-                        path="/users/edit/:id"
-                        element={<EditUSer />}
-                    />
-                    <Route exact path="/users/:id" element={<User />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <NavBar setfilter={setFilter} />
+        <Routes>
+          <Route path="/" element={<Home searchThis={baseFilter} />} />
+          <Route path="/users/add" element={<AddUser />} />
+          <Route exact path="/users/edit/:id" element={<EditUSer />} />
+          <Route exact path="/users/:id" element={<User />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -3,41 +3,43 @@ import { Link, NavLink } from "react-router-dom";
 import "./NavBar.css";
 import { users } from "../../db.json";
 const NavBar = ({ setfilter }) => {
-    const [search, setSearch] = useState("");
-    const handleChange = (e) => {
-        setSearch(e.target.value);
-        setfilter(e.target.value);
-    };
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark ">
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/">
-                            Home
-                        </NavLink>
-                    </li>
-                </ul>
-                <div className="search-item">
-                    <form class="form-inline my-2 my-lg-0">
-                        <input
-                            style={{ width: "500px", margin: "auto 200px" }}
-                            class="form-control mr-sm-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                            onChange={handleChange}
-                        />
-                    </form>
-                </div>
-            </div>
-            <div>
-                <Link className="btn btn-outline-light" to="/users/add">
-                    Add User
-                </Link>
-            </div>
-        </nav>
-    );
+	const [search, setSearch] = useState("");
+	const handleChange = (e) => {
+		setSearch(e.target.value);
+		setfilter(e.target.value);
+	};
+	return (
+		<>
+			<nav className="navbar navbar-expand-lg navbar-dark ">
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav mr-auto">
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/">
+								Home
+							</NavLink>
+						</li>
+					</ul>
+					<div className="search-item">
+						<form class="form-inline my-2 my-lg-0">
+							<input
+								style={{ width: "500px", margin: "auto 200px" }}
+								class="form-control mr-sm-2"
+								type="search"
+								placeholder="Search"
+								aria-label="Search"
+								onChange={handleChange}
+							/>
+						</form>
+					</div>
+				</div>
+				<div>
+					<Link className="btn btn-outline-light" to="/users/add">
+						Add User
+					</Link>
+				</div>
+			</nav>
+		</>
+	);
 };
 
 export default NavBar;
